d569622a32c3aeb658884908fb94ce0978106c69
Igor Korolevich <ikorolevich4@gmail.com>
2024-01-19 20:59:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module5_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
